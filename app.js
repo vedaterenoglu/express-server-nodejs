3ce2d82b7a74c152e2825e67829b920b6fa925f0
Vedat Erenoglu <evedat@gmail.com>
2019-07-04 13:48:31 +0200

    5

--- app.js
+++ app.js
@@ -2,17 +2,18 @@
 We import express as well as our router and then we add the body parser and router to express and specify which port (in this example port 4200) we want to run on.
 */
 
-import express from 'express';
-import bodyParser from 'body-parser';
-import router from './routes/index';
-let app = express();
+import express from 'express'
+import bodyParser from 'body-parser'
+import router from './routes/index'
+let app = express()
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 
-app.use('/', router);
+app.use('/', router)
 
 app.listen(4200, function () {
-    console.log('app listening on port 4200!')
-});
+    console.log(`Express server is on!!!
+App listening on port 4200!`)
+})
--- models/book.model.js
+++ models/book.model.js
@@ -5,15 +5,15 @@ We create a model for JSON output
 
 export default class Book {
   constructor(bookName, author, bookType){
-     this.bookName = bookName;
-     this.author = author;
-     this.bookType = bookType;
+     this.bookName = bookName
+     this.author = author
+     this.bookType = bookType
    }
 
    getAuthor(){
-     return this.author;
+     return this.author
    }
    getBookName(){
-     return this.bookName;
+     return this.bookName
    }
 }
